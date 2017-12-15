f9fcc5faa6d449049a3d2920f7d2f67867f2a404
Marcio Lucas <marciioluucas@gmail.com>
2017-12-15 10:52:12 -0200

    Fazendo a autenticação de módulos encriptados.

--- src/util/security/BCrypt.ts
+++ src/util/security/BCrypt.ts
@@ -5,17 +5,25 @@ class BCrypt {
     private contextHash: String;
     private _contexto: String;
 
+    /*
+    * O modulo manda a requisição com a hash de contexto encriptada
+    */
     constructor(contextHash: String) {
         this.contextHash = contextHash;
     }
 
+    /*
+   * O módulo auth lê os contextos cadastrados no arquivo contexts.json
+   */
     private lerContextos() {
         return JSON.parse(fs.readFileSync('./contexts.json', 'utf8'))
     }
 
+    /*
+   * Depois precisa chamar este método, que verifica se o contexto solicitado está cadastrado no arquivo contexts.json
+   */
     public verificar() {
         let contextos = this.lerContextos().contexts;
-        let r = false;
 
         for (let i = 0; i < contextos.length; i++) {
             if (bcrypt.compareSync(contextos[i].module, this.contextHash) == true) {
@@ -26,7 +34,9 @@ class BCrypt {
         return false;
     }
 
-
+    /*
+       * Se precisar pegar o contexto depois, (para fazer o arquivo de configuração do banco, etc.)
+       */
     get contexto(): String {
         return this._contexto;
     }
