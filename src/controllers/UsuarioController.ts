0e4e24110a558b5201a099ab3da54571170af8b8
Marcio Lucas <marciioluucas@gmail.com>
2017-12-19 09:12:16 -0200

    Colocando a biblioteca de status http

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -4,8 +4,14 @@ import {UsuarioService} from '../service/UsuarioService';
 export class UsuarioController {
 
     static login = function (req, res) {
-        return res.json(new UsuarioService(req.params.context).validar(req.body))
+
+     return new UsuarioService(req.params.context).validar(req.body, (msg) => {
+
+          return res.status.badRequest(msg)
+        });
+
     };
+
 
     static faz = function (req, res) {
         return res.json(new BCrypt().construir(req.params.context))
--- src/service/UsuarioService.ts
+++ src/service/UsuarioService.ts
@@ -1,6 +1,5 @@
 import Usuario from "../models/Usuario";
 import {validate} from "class-validator";
-import {error} from "util";
 
 export class UsuarioService {
 
@@ -10,20 +9,19 @@ export class UsuarioService {
         this._context = context;
     }
 
-   validar(value: Object) {
+    validar(value: Object, callback) {
         let pessoa = new Usuario(value);
-        validate(pessoa).then(errors => {
+        let mensagem: any = [];
+        return validate(pessoa).then(errors => {
             if (errors.length > 0) {
-                let mensagem = [];
                 errors.forEach(function (a) {
                     for (let inc in a.constraints) {
                         mensagem.push(a.constraints[inc])
                     }
                 });
-                console.log({msg:mensagem});
-                return {msg: mensagem}
             }
-            return {msg: "validou"}
-        })
+            callback({msg: mensagem})
+        });
+
     }
 }
